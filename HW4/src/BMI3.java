d7424e8fbd947d2c2bdd404d37e9539a90a59aca
SkiingxMoose <jodesmond@live.com>
2015-11-20 19:40:17 -0500

    Merged (Semi-merged) Saa's Changes

---
+++ HW4/src/BMI3.java
@@ -0,0 +1,44 @@
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+
+class BMI3 implements IBMIProbs {
+	BMI3(){}
+
+	//using a filter
+	@Override
+	public BMISummary bmiReport(final LinkedList<PHR> phrs) {
+
+
+		final LinkedList<String> under = filterUnderWeight(phrs);
+		final LinkedList<String>  healthy = filterHealthy(phrs);
+		final LinkedList<String> overweight = filterOver(phrs);
+		final LinkedList<String> obese = filterObese(phrs);
+
+		return new BMISummary(under, healthy, overweight, obese);
+	}
+
+	private LinkedList<String> filterObese(final LinkedList<PHR> phrs) {
+		return phrs.stream().filter((el) -> (BMI(el.weight, el.height) >= 30)).map(el-> el.name).collect(Collectors.toCollection(LinkedList::new));
+	}
+
+	private LinkedList<String> filterOver(final LinkedList<PHR> phrs) {
+		return phrs.stream().filter((el) -> (BMI(el.weight, el.height) >= 25 &&
+				BMI(el.weight, el.height) <  30)).map(el-> el.name).collect(Collectors.toCollection(LinkedList::new));
+	}
+
+	private LinkedList<String> filterHealthy(final LinkedList<PHR> phrs) {
+		return phrs.stream().filter((el) -> (BMI(el.weight, el.height) >= 18.5 &&
+				BMI(el.weight, el.height) < 25)).map(el-> el.name).collect(Collectors.toCollection(LinkedList::new));
+	}
+
+	private LinkedList<String> filterUnderWeight(final LinkedList<PHR> phrs) {
+		return phrs.stream().filter((el) -> (BMI(el.weight, el.height) < 18.5)).map(el-> el.name).collect(Collectors.toCollection(LinkedList::new));
+	}
+
+	double BMI (final double weight, final double height)
+	{
+		return weight / (height*height);
+	}
+
+
+}
--- HW4/src/PHR.java
+++ HW4/src/PHR.java
@@ -21,5 +21,63 @@ class PHR {
 		return weight / (height * height);
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + heartRate;
+		long temp;
+		temp = Double.doubleToLongBits(height);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		temp = Double.doubleToLongBits(weight);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+
+
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final PHR other = (PHR) obj;
+		if (heartRate != other.heartRate)
+			return false;
+		if (Double.doubleToLongBits(height) != Double
+				.doubleToLongBits(other.height))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (Double.doubleToLongBits(weight) != Double
+				.doubleToLongBits(other.weight))
+			return false;
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "PHR [" + (name != null ? "name=" + name + ", " : "")
+				+ "height=" + height + ", weight=" + weight + ", heartRate="
+				+ heartRate + "]";
+	}
+
+
 
 }
