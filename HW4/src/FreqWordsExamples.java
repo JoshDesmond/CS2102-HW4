82bde224f899b1b99ba8b29a80fb6d30ed445dd6
SaahilClaypool <saahil@claypools.org>
2015-11-21 22:27:58 -0500

    made changes to most frequent

--- HW4/src/FreqWordsExamples.java
+++ HW4/src/FreqWordsExamples.java
@@ -1,6 +1,10 @@
 
 import tester.*;
 
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
 class FreqWordsExamples {
   IFreqWordsProbs F;
   
@@ -8,7 +12,9 @@ class FreqWordsExamples {
     this.F = F;
   }
   
-  boolean testFoo (Tester t) {
-    return t.checkExpect(F.frequentWords(null), null);
+  boolean testSimple (Tester t) {
+    LinkedList<String>  test = new LinkedList<String>(Arrays.asList("hello","my", "name","name","name", "my" ,"I"));
+    LinkedList<String>  answers = new LinkedList<String>(Arrays.asList("name", "my", "I"));
+    return t.checkExpect(F.frequentWords(test), answers);
   }
 }
