2d04132ed2c0f2a241d0f556221944619ba50837
SkiingxMoose <jodesmond@live.com>
2015-11-24 02:27:26 -0500

    quick cleanup of BMIExamples

--- HW4/src/BMIExamples.java
+++ HW4/src/BMIExamples.java
@@ -4,70 +4,69 @@ import java.util.LinkedList;
 import tester.Tester;
 
 class BMIExamples {
-	IBMIProbs B;
+    IBMIProbs B;
 
-	BMIExamples(final IBMIProbs b) {
-		this.B = b;
-	}
+    BMIExamples(final IBMIProbs b) {
+        this.B = b;
+    }
 
-	public static LinkedList<PHR> getRecords() {
+    /**
+     * Test function using all of the data retrieved from this class.
+     */
+    boolean testBMI1(final Tester t) {
+        return t.checkExpect(B.bmiReport(getRecords()), getBMIData());
+    }
 
-		final PHR p1 = buildPHR("p1", 1.0, 1.0);
-		final PHR p2 = buildPHR("p2", 18.4, 1.0);
-		final PHR p3 = buildPHR("p3", 18.6, 1.0);
-		final PHR p4 = buildPHR("p4", 50.0, 1.0);
-		final PHR p5 = buildPHR("p5", 50.0, 1.0);
+    /**
+     * Slight Modification to {@link #testBMI1(Tester)}. Also more of an
+     * integration test.
+     */
+    boolean testBMI2(final Tester t) {
+        final LinkedList<PHR> in = getRecords();
+        final BMISummary out = getBMIData();
 
-		return new LinkedList<PHR>(Arrays.asList(p1, p2, p3, p4, p5));
-	}
+        final PHR p = buildPHR("p", 100.1, 2.0);
+        in.add(p);
+        out.over.add("p");
 
-	// Convenience constructor
-	private static PHR buildPHR(final String name, final double weight,
-			final double height) {
-		return new PHR(name, height, weight, 120);
-	}
+        // check for failure
+        if (!t.checkExpect(B.bmiReport(in), out))
+            return t.fail();
 
-	/**
-	 * get list of BMIData: [p1,p2], [p3], [], [p4]
-	 */
-	private static BMISummary getBMIData() {
-		// @formatter:off
-		final LinkedList<String> under = new LinkedList<String>(Arrays.asList("p1", "p2"));
-		final LinkedList<String> health = new LinkedList<String>(Arrays.asList("p3"));
-		final LinkedList<String> over = new LinkedList<String>();
-		final LinkedList<String> obese = new LinkedList<String>(Arrays.asList("p4", "p5"));
-		// @formatter:off
-		return new BMISummary(under, health, over, obese);
-	}
+        // then modify data, to ensure no modifying any of given lists.
+        in.remove(p);
+        out.over.remove("p");
 
-	/**
-	 * Test function using all of the data retrieved from this class.
-	 */
-	boolean testBMI1(final Tester t) {
-		return t.checkExpect(B.bmiReport(getRecords()),getBMIData());
-	}
+        return t.checkExpect(B.bmiReport(in), out);
+    }
 
-	/**
-	 * Slight Modification to above test. Also more of an integration test.
-	 */
-	boolean testBMI2(final Tester t) {
-		final LinkedList<PHR> in = getRecords();
-		final BMISummary out = getBMIData();
+    public static LinkedList<PHR> getRecords() {
 
-		final PHR p = buildPHR("p",100.1,2.0);
-		in.add(p);
-		out.over.add("p");
+        final PHR p1 = buildPHR("p1", 1.0, 1.0);
+        final PHR p2 = buildPHR("p2", 18.4, 1.0);
+        final PHR p3 = buildPHR("p3", 18.6, 1.0);
+        final PHR p4 = buildPHR("p4", 50.0, 1.0);
+        final PHR p5 = buildPHR("p5", 50.0, 1.0);
 
-		// check for failure
-		if (!t.checkExpect(B.bmiReport(in),out))
-			return t.fail();
+        return new LinkedList<PHR>(Arrays.asList(p1, p2, p3, p4, p5));
+    }
 
-		// then modify data, to ensure that bmiReport isn't modifying any of my lists.
-		in.remove(p);
-		out.over.remove("p");
+    // Convenience constructor
+    private static PHR buildPHR(final String name, final double weight,
+            final double height) {
+        return new PHR(name, height, weight, 120);
+    }
 
-		return t.checkExpect(B.bmiReport(in),out);
-
-	}
-
+    /**
+     * get list of BMIData: [p1,p2], [p3], [], [p4]
+     */
+    private static BMISummary getBMIData() {
+        // @formatter:off
+        final LinkedList<String> under = new LinkedList<String>(Arrays.asList("p1", "p2"));
+	    final LinkedList<String> health = new LinkedList<String>(Arrays.asList("p3"));
+	    final LinkedList<String> over = new LinkedList<String>();
+	    final LinkedList<String> obese = new LinkedList<String>(Arrays.asList("p4", "p5"));
+	    // @formatter:off
+	    return new BMISummary(under, health, over, obese);
+	   }
 }
