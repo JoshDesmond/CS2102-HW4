a775324fc01ee4ff53f12d0da12ab54ba592f4b8
SkiingxMoose <jodesmond@live.com>
2015-11-24 01:33:26 -0500

    Finished Earthquake?

--- HW4/src/Earthquake1.java
+++ HW4/src/Earthquake1.java
@@ -4,88 +4,107 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 class Earthquake1 implements IEarthquakeProbs {
-	Earthquake1() {
-	}
+    Earthquake1() {
+    }
 
-	private static Function<? super LinkedList<Double>, MaxHzReport> MAP_FUNCTION =
-			(list -> {
-				for (double d : list) {
+    @Override
+    public LinkedList<MaxHzReport> dailyMaxForMonth(LinkedList<Double> data,
+            int month) {
 
-				}
-			})
+        // First split data.
+        LinkedList<LinkedList<Double>> splitData = splitLists(data);
 
-			@Override
-			public LinkedList<MaxHzReport> dailyMaxForMonth(LinkedList<Double> data,
-					int month) {
+        // Then filter through and retrieve only data for given month.
+        splitData = splitData.stream().filter(getPredicate(month))
+                .collect(Collectors.toCollection(LinkedList::new));
 
-		// First split data.
-		LinkedList<LinkedList<Double>> splitData = splitLists(data);
-		// Then filter through and retrieve only data for given month.
-		splitData = splitData.stream().filter(getPredicate(month))
-				.collect(Collectors.toCollection(LinkedList::new));
+        // Then convert these dailyDataLists into MaxHzReads.
+        Function<LinkedList<Double>, MaxHzReport> mapFunction = getMapFunction();
+        return splitData.stream().map(mapFunction)
+                .collect(Collectors.toCollection(LinkedList::new));
+    }
 
-		// Then convert these dailyDataLists into MaxHzReads.
-		return splitData.stream().map(getMapFunction())
-				.collect(Collectors.toCollection(LinkedList::new));
-	}
+    /**
+     * Returns a function which maps a dailyDataList into a MaxHzReads.
+     * DailyDataLists are in the form: <i>{yyyymmdd, val1, val2, ... }</i>.
+     * Current implementation has the side effect of destroying the given list
+     * it's working on.
+     * 
+     * @return Function mapping dailyData to MaxHzReport
+     */
+    private static Function<LinkedList<Double>, MaxHzReport> getMapFunction() {
+        // Define function using -> notation.
+        Function<LinkedList<Double>, MaxHzReport> function = list -> {
+            double dateDouble = list.pop();
+            int day = EarthquakeUtils.dayOfDate(dateDouble);
 
-	private Function<? super LinkedList<Double>, MaxHzReport> getMapFunction() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+            // Find best
+            double maxRead = 0.0;
+            for (double read : list) {
+                if (read > maxRead) {
+                    maxRead = read;
+                }
+            }
 
-	/**
-	 * Converts raw sensor data into split data
-	 * 
-	 * @return Builds lists of daily reading lists. Each list within total list
-	 *         will begin with a date.
-	 */
-	private LinkedList<LinkedList<Double>> splitLists(
-			LinkedList<Double> sensorData) {
-		// Set up iterator
-		LinkedList<LinkedList<Double>> splitList = new LinkedList<LinkedList<Double>>();
-		LinkedList<Double> currentList = new LinkedList<Double>();
+            return new MaxHzReport(day, maxRead);
+        };
 
-		currentList.add(sensorData.pop());
+        return function;
+    }
 
-		while (!sensorData.isEmpty()) {
-			Double peek = sensorData.peek();
+    /**
+     * Converts raw sensor data into split data
+     * 
+     * @return Builds lists of daily reading lists. Each list within total list
+     *         will begin with a date.
+     */
+    private LinkedList<LinkedList<Double>> splitLists(
+            LinkedList<Double> sensorData) {
+        // Set up iterator
+        LinkedList<LinkedList<Double>> splitList = new LinkedList<LinkedList<Double>>();
+        LinkedList<Double> currentList = new LinkedList<Double>();
 
-			if (EarthquakeUtils.isOfTypeDate(peek)) {
-				// Then terminate currentList and start anew.
-				splitList.add(currentList);
-				currentList = new LinkedList<Double>();
-			} else if (!EarthquakeUtils.isOfTypeFreq(peek)) {
-				throw new IllegalArgumentException(String.format(
-						"Given argument within sensor data list was neither of type "
-								+ "frequency nor date: %s", peek));
-			}
+        currentList.add(sensorData.pop());
 
-			currentList.add(sensorData.pop());
-		}
+        while (!sensorData.isEmpty()) {
+            Double peek = sensorData.peek();
 
-		splitList.add(currentList);
-		return splitList;
-	}
+            if (EarthquakeUtils.isOfTypeDate(peek)) {
+                // Then terminate currentList and start anew.
+                splitList.add(currentList);
+                currentList = new LinkedList<Double>();
+            } else if (!EarthquakeUtils.isOfTypeFreq(peek)) {
+                throw new IllegalArgumentException(String
+                        .format("Given argument within sensor data list was neither of type "
+                                + "frequency nor date: %s", peek));
+            }
 
-	/**
-	 * Returns a predicate which determines which month this parsed data belongs
-	 * to.
-	 * 
-	 * @param month
-	 *            int between 1 and 12 inclusive
-	 * @return Predicate function of (LinkedList<Double> -> boolean).
-	 */
-	public static Predicate<? super LinkedList<Double>> getPredicate(int month) {
-		if (0 >= month || 12 < month) {
-			throw new IllegalArgumentException(String.format(
-					"Given month is not valid %s", month));
-		}
+            currentList.add(sensorData.pop());
+        }
 
-		Predicate<? super LinkedList<Double>> func = (list -> EarthquakeUtils
-				.monthOfDate(list.peek()) == month);
+        splitList.add(currentList);
+        return splitList;
+    }
 
-		return func;
-	}
+    /**
+     * Returns a predicate which determines which month this parsed data belongs
+     * to.
+     * 
+     * @param month
+     *            int between 1 and 12 inclusive
+     * @return Predicate function of (LinkedList<Double> -> boolean).
+     */
+    public static Predicate<? super LinkedList<Double>> getPredicate(
+            int month) {
+        if (0 >= month || 12 < month) {
+            throw new IllegalArgumentException(
+                    String.format("Given month is not valid %s", month));
+        }
+
+        Predicate<? super LinkedList<Double>> func = (list -> EarthquakeUtils
+                .monthOfDate(list.peek()) == month);
+
+        return func;
+    }
 
 }
--- HW4/src/Earthquake2.java
+++ HW4/src/Earthquake2.java
@@ -7,81 +7,81 @@ import java.util.LinkedList;
  * @author Josh Desmond
  */
 class Earthquake2 implements IEarthquakeProbs {
-	Earthquake2() {
-	}
+    Earthquake2() {
+    }
 
-	@Override
-	public LinkedList<MaxHzReport> dailyMaxForMonth(
-			final LinkedList<Double> data, final int month) {
+    @Override
+    public LinkedList<MaxHzReport> dailyMaxForMonth(
+            final LinkedList<Double> data, final int month) {
 
-		if (data.isEmpty()) {
-			return new LinkedList<MaxHzReport>();
-		}
+        if (data.isEmpty()) {
+            return new LinkedList<MaxHzReport>();
+        }
 
-		EarthquakeReportsByMonth reportsByMonth = convertToReportsByMonthMap(data);
+        EarthquakeReportsByMonth reportsByMonth = convertToReportsByMonthMap(
+                data);
 
-		return reportsByMonth.get(month);
-	}
+        return reportsByMonth.get(month);
+    }
 
-	/**
-	 * Converts a sensorData list input to an indexed map of month ->
-	 * MaxHzReport
-	 * 
-	 * @param sensorData
-	 *            raw input data
-	 * @return A Map of int month to MaxHzReports
-	 */
-	public EarthquakeReportsByMonth convertToReportsByMonthMap(
-			final LinkedList<Double> sensorData) {
-		// Instantiate reportsByMonth
-		EarthquakeReportsByMonth reportsByMonth = EarthquakeReportsByMonth
-				.instantiateMap();
+    /**
+     * Converts a sensorData list input to an indexed map of month ->
+     * MaxHzReport
+     * 
+     * @param sensorData
+     *            raw input data
+     * @return A Map of int month to MaxHzReports
+     */
+    public EarthquakeReportsByMonth convertToReportsByMonthMap(
+            final LinkedList<Double> sensorData) {
+        // Instantiate reportsByMonth
+        EarthquakeReportsByMonth reportsByMonth = EarthquakeReportsByMonth
+                .instantiateMap();
 
-		// split data in days.
-		LinkedList<LinkedList<Double>> splitData = splitLists(sensorData);
+        // split data in days.
+        LinkedList<LinkedList<Double>> splitData = splitLists(sensorData);
 
-		// Then, for each day, add it to the growing reportsByMonth.
-		for (LinkedList<Double> dailyReadings : splitData) {
-			reportsByMonth.processDailyData(dailyReadings);
-		}
+        // Then, for each day, add it to the growing reportsByMonth.
+        for (LinkedList<Double> dailyReadings : splitData) {
+            reportsByMonth.processDailyData(dailyReadings);
+        }
 
-		// Return data.
-		return reportsByMonth;
-	}
+        // Return data.
+        return reportsByMonth;
+    }
 
-	/**
-	 * Converts raw sensor data into split data
-	 * 
-	 * @return Builds lists of daily reading lists. Each list within total list
-	 *         will begin with a date.
-	 */
-	private LinkedList<LinkedList<Double>> splitLists(
-			LinkedList<Double> sensorData) {
-		// Set up iterator
-		LinkedList<LinkedList<Double>> splitList = new LinkedList<LinkedList<Double>>();
-		LinkedList<Double> currentList = new LinkedList<Double>();
+    /**
+     * Converts raw sensor data into split data
+     * 
+     * @return Builds lists of daily reading lists. Each list within total list
+     *         will begin with a date.
+     */
+    private LinkedList<LinkedList<Double>> splitLists(
+            LinkedList<Double> sensorData) {
+        // Set up iterator
+        LinkedList<LinkedList<Double>> splitList = new LinkedList<LinkedList<Double>>();
+        LinkedList<Double> currentList = new LinkedList<Double>();
 
-		currentList.add(sensorData.pop());
+        currentList.add(sensorData.pop());
 
-		while (!sensorData.isEmpty()) {
-			Double peek = sensorData.peek();
+        while (!sensorData.isEmpty()) {
+            Double peek = sensorData.peek();
 
-			if (EarthquakeUtils.isOfTypeDate(peek)) {
-				// Then terminate currentList and start anew.
-				splitList.add(currentList);
-				currentList = new LinkedList<Double>();
-			} else if (!EarthquakeUtils.isOfTypeFreq(peek)) {
-				throw new IllegalArgumentException(String.format(
-						"Given argument within sensor data list was neither of type "
-								+ "frequency nor date: %s", peek));
-			}
+            if (EarthquakeUtils.isOfTypeDate(peek)) {
+                // Then terminate currentList and start anew.
+                splitList.add(currentList);
+                currentList = new LinkedList<Double>();
+            } else if (!EarthquakeUtils.isOfTypeFreq(peek)) {
+                throw new IllegalArgumentException(String
+                        .format("Given argument within sensor data list was neither of type "
+                                + "frequency nor date: %s", peek));
+            }
 
-			currentList.add(sensorData.pop());
-		}
+            currentList.add(sensorData.pop());
+        }
 
-		splitList.add(currentList);
-		return splitList;
-	}
-
+        splitList.add(currentList);
+        return splitList;
+    }
 
 }
