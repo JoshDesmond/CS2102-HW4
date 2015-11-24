697d1fd49377ab68541e0ad9bef7ae487dab23a6
SkiingxMoose <jodesmond@live.com>
2015-11-24 02:33:10 -0500

    fixed a bug (old println statement caused error in new test).

--- HW4/src/BMI4.java
+++ HW4/src/BMI4.java
@@ -1,44 +1,35 @@
 import java.util.LinkedList;
 
 class BMI4 implements IBMIProbs {
-  BMI4(){}
-  // single loop
-  public BMISummary bmiReport(LinkedList<PHR> phrs) {
-    System.out.println(BMI(phrs.get(2)));
-    LinkedList<String> under = new LinkedList<String>();
-    LinkedList<String> healthy = new LinkedList<String>();
-    LinkedList<String> over  = new LinkedList<String>();
-    LinkedList<String> obese = new LinkedList<String>();
-
-    for (int i = 0; i < phrs.size(); i++) {
-
-      if(BMI(phrs.get(i))< 18.5)
-      {
-        under.add(phrs.get(i).name);
-      }
-      else if(BMI(phrs.get(i))>= 18.5&&
-              BMI(phrs.get(i))< 25)
-      {
-        healthy.add(phrs.get(i).name);
-      }
-      else if(BMI(phrs.get(i))>= 25 &&
-              BMI(phrs.get(i)) < 30)
-      {
-        over.add(phrs.get(i).name);
-      }
-      else if (BMI(phrs.get(i))>= 30)
-      {
-        obese.add(phrs.get(i).name);
-      }
-
-
+    BMI4() {
     }
-    return new BMISummary(under, healthy, over, obese);
-  }
-  double BMI (PHR phr)
-  {
-    double weight = phr.weight;
-    double height = phr.height;
-    return weight / (height*height);
-  }
+
+    // single loop
+    public BMISummary bmiReport(LinkedList<PHR> phrs) {
+        LinkedList<String> under = new LinkedList<String>();
+        LinkedList<String> healthy = new LinkedList<String>();
+        LinkedList<String> over = new LinkedList<String>();
+        LinkedList<String> obese = new LinkedList<String>();
+
+        for (int i = 0; i < phrs.size(); i++) {
+
+            if (BMI(phrs.get(i)) < 18.5) {
+                under.add(phrs.get(i).name);
+            } else if (BMI(phrs.get(i)) >= 18.5 && BMI(phrs.get(i)) < 25) {
+                healthy.add(phrs.get(i).name);
+            } else if (BMI(phrs.get(i)) >= 25 && BMI(phrs.get(i)) < 30) {
+                over.add(phrs.get(i).name);
+            } else if (BMI(phrs.get(i)) >= 30) {
+                obese.add(phrs.get(i).name);
+            }
+
+        }
+        return new BMISummary(under, healthy, over, obese);
+    }
+
+    double BMI(PHR phr) {
+        double weight = phr.weight;
+        double height = phr.height;
+        return weight / (height * height);
+    }
 }
