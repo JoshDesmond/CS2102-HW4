95cfd272f920ba3428a0c8782ee54e23925adc8d
SkiingxMoose <jodesmond@live.com>
2015-11-24 02:18:58 -0500

    Fixed Broken DataSmooth1
    Also did a bit of refactoring within it.

--- HW4/src/DataSmooth1.java
+++ HW4/src/DataSmooth1.java
@@ -3,21 +3,39 @@ import java.util.LinkedList;
 import java.util.stream.Collectors;
 
 class DataSmooth1 implements IDataSmoothProbs {
-  DataSmooth1(){}
-  
-  public LinkedList<Double> dataSmooth(LinkedList<PHR> phrs) {
-    LinkedList<Integer> original = phrs.stream().map(el -> el.heartRate).collect(Collectors.toCollection(LinkedList::new));
-    LinkedList<Double> changes = new LinkedList<>();
-    changes.add(new Double(original.get(0)));
-    for (int i = 1; i < original.size() -1; i++) {
-      Integer ints = original.get(i);
-        changes.add(i, average(original.get(i-1) ,original.get(i), original.get(i+1)));
+    DataSmooth1() {
     }
-    changes.add(new Double(original.get(original.size()-1)));
-    return changes;
-  }
-  public Double average(int one, int two, int three)
-  {
-    return new Double((one + two + three) / 3.);
-  }
+
+    public LinkedList<Double> dataSmooth(LinkedList<PHR> phrs) {
+        LinkedList<Double> original = mapToHeartRates(phrs);
+
+        if (phrs.size() <= 2) {
+            return original;
+        }
+
+        LinkedList<Double> changes = new LinkedList<>();
+        changes.add(new Double(original.get(0)));
+        for (int i = 1; i < original.size() - 1; i++) {
+            changes.add(i, average(original.get(i - 1), original.get(i),
+                    original.get(i + 1)));
+        }
+        changes.add(new Double(original.get(original.size() - 1)));
+        return changes;
+    }
+
+    public Double average(double double1, double double2, double double3) {
+        return (double1 + double2 + double3) / 3.;
+    }
+
+    /**
+     * Maps list of phrs to list of just heartrates.
+     * 
+     * @param phrs
+     *            LinkedList of PHR's
+     * @return LinkedList of doubles
+     */
+    private LinkedList<Double> mapToHeartRates(LinkedList<PHR> phrs) {
+        return phrs.stream().map(el -> el.heartRate * 1.0)
+                .collect(Collectors.toCollection(LinkedList::new));
+    }
 }
--- HW4/src/DataSmoothExamples.java
+++ HW4/src/DataSmoothExamples.java
@@ -1,35 +1,80 @@
 
-import tester.*;
-
 import java.util.Arrays;
 import java.util.LinkedList;
 
+import tester.TestMethod;
+import tester.Tester;
+
 class DataSmoothExamples {
-  IDataSmoothProbs D;
-  
-  DataSmoothExamples(IDataSmoothProbs D) {
-    this.D = D;
-  }
-  
-  boolean testGiven  (Tester t)
-  {
-    //95 102 98 88 105
-    LinkedList<PHR> phrs = new LinkedList<PHR>(Arrays.asList(
-            new PHR("1", new Integer(3),new Integer(3), new Integer(95)),
-            new PHR("1", new Integer(3),new Integer(3), new Integer(102)),
-            new PHR("1", new Integer(3),new Integer(3), new Integer(98)),
-            new PHR("1", new Integer(3),new Integer(3), new Integer(88)),
-            new PHR("1", new Integer(3),new Integer(3), new Integer(105))
-    ));
-//  95 98.33 96 97 105
-    LinkedList<Double> answers = new LinkedList<Double>(Arrays.asList(
-            new Double(95),
-            new Double((95 + 102 + 98) / 3.),
-            new Double(96),
-            new Double(97),
-            new Double(105)
-    ));
-    return t.checkExpect(D.dataSmooth(phrs), answers);
-  }
-  
+    IDataSmoothProbs D;
+
+    DataSmoothExamples(IDataSmoothProbs D) {
+        this.D = D;
+    }
+
+    @TestMethod
+    boolean testGiven(Tester t) {
+
+        LinkedList<PHR> phrs = getInputData();
+
+        LinkedList<Double> answers = getOutputData();
+        return t.checkExpect(D.dataSmooth(phrs), answers);
+
+    }
+
+    @TestMethod
+    boolean testConservationOfInput(Tester t) {
+        LinkedList<PHR> phrs = getInputData();
+        LinkedList<Double> answers = getOutputData();
+
+        if (!t.checkExpect(D.dataSmooth(phrs), answers)) {
+            return t.fail();
+        }
+
+        return t.checkExpect(D.dataSmooth(phrs), answers);
+    }
+
+    @TestMethod
+    public boolean testSingleAndDoubleInput(Tester t) {
+        LinkedList<PHR> phrs = new LinkedList<PHR>(
+                Arrays.asList(new PHR("name", 3, 5, 100)));
+        LinkedList<Double> answers = new LinkedList<Double>(
+                Arrays.asList(100d));
+
+        if (!t.checkExpect(D.dataSmooth(phrs), answers)) {
+            return t.fail();
+        }
+
+        // Now add something to both lists:
+        phrs.add(new PHR("name2", 4, 2, 110));
+        answers.add(110d);
+        return t.checkExpect(D.dataSmooth(phrs), answers);
+    }
+
+    /** 95 98.33 96 97 105 */
+    private LinkedList<Double> getOutputData() {
+     // @formatter:off
+        return new LinkedList<Double>(Arrays.asList(
+                new Double(95),
+                new Double((95 + 102 + 98) / 3.),
+                new Double(96),
+                new Double(97),
+                new Double(105)
+                ));
+     // @formatter:on
+    }
+
+    /** 95 102 98 88 105 */
+    private LinkedList<PHR> getInputData() {
+     // @formatter:off
+        return new LinkedList<PHR>(Arrays.asList(
+                new PHR("1", 3,3, new Integer(95)),
+                new PHR("1", 3,3, new Integer(102)),
+                new PHR("1", 3,3, new Integer(98)),
+                new PHR("1", 3,3, new Integer(88)),
+                new PHR("1", 3,3, new Integer(105))
+                ));
+     // @formatter:on
+    }
+
 }
