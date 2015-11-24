c29645348453ebc1bb21314c35dafafa2c0da196
SkiingxMoose <jodesmond@live.com>
2015-11-24 03:45:48 -0500

    final touchups?

--- HW4/src/MostFrequentWords2.java
+++ HW4/src/MostFrequentWords2.java
@@ -3,91 +3,95 @@ import java.util.Arrays;
 import java.util.LinkedList;
 
 class MostFrequentWords2 implements IFreqWordsProbs {
-  MostFrequentWords2(){}
-  // single pass
-  public LinkedList<String> frequentWords(LinkedList<String> words) {
-    // store temporary string and frequency counts
-    String maxString = "";
-    int maxCount = 0;
+    MostFrequentWords2() {
+    }
 
-    String medString = "";
-    int medCount = 0;
+    // single pass
+    public LinkedList<String> frequentWords(LinkedList<String> words) {
+        // store temporary string and frequency counts
+        String maxString = "";
+        int maxCount = 0;
 
-    String minString = "";
-    int minCount = 0;
+        String medString = "";
+        int medCount = 0;
 
-    String curString = "";
-    int curCount = 0;
-    // sort list in alphabetic order
-    words.sort(String::compareTo);
-    for(String s : words)
-    {
-      // any mismatch signals the end of the current series of words
-      if(!s.equals(curString))
-      {
-        // shift down words with lower counts
-        if(isLarger(curCount, minCount,curString.length(), minString.length()))
-        {
-          minCount = curCount;
-          minString = curString;
+        String minString = "";
+        int minCount = 0;
+
+        String curString = "";
+        int curCount = 0;
+        // sort list in alphabetic order
+        words.sort(String::compareTo);
+        for (String s : words) {
+            // any mismatch signals the end of the current series of words
+            if (!s.equals(curString)) {
+                // shift down words with lower counts
+                if (isLarger(curCount, minCount, curString.length(),
+                        minString.length())) {
+                    minCount = curCount;
+                    minString = curString;
+                }
+                if (isLarger(curCount, medCount, curString.length(),
+                        medString.length())) {
+                    minCount = medCount;
+                    minString = medString;
+
+                    medString = curString;
+                    medCount = curCount;
+                }
+                if (isLarger(curCount, maxCount, curString.length(),
+                        maxString.length())) {
+
+                    medString = maxString;
+                    medCount = maxCount;
+
+                    maxString = curString;
+                    maxCount = curCount;
+                }
+                curString = s;
+                curCount = 1;
+
+            } else
+                curCount += 1; // increase the counter for the current series of
+                               // words by one
         }
-        if(isLarger(curCount,medCount,curString.length(),medString.length()))
-        {
-          minCount = medCount;
-          minString = medString;
-
-          medString = curString;
-          medCount = curCount;
+        // add the last element held
+        if (isLarger(curCount, minCount, curString.length(),
+                minString.length())) {
+            minCount = curCount;
+            minString = curString;
         }
-        if(isLarger(curCount,maxCount,curString.length(),maxString.length()))
-        {
 
+        if (isLarger(curCount, medCount, curString.length(),
+                medString.length())) {
+            minCount = medCount;
+            minString = medString;
 
-          medString = maxString;
-          medCount = maxCount;
-
-          maxString = curString;
-          maxCount = curCount;
+            medString = curString;
+            medCount = curCount;
         }
-        curString = s;
-        curCount =1;
 
-      }
-      else
-        curCount +=1; // increase the counter for the current series of words by one
-    }
-    //add the last element held
-    if(isLarger(curCount,minCount,curString.length(), minString.length()))
-    {
-      minCount = curCount;
-      minString = curString;
-    }
+        if (isLarger(curCount, maxCount, curString.length(),
+                maxString.length())) {
 
-    if(isLarger(curCount, medCount,curString.length(), medString.length()))
-    {
-      minCount = medCount;
-      minString = medString;
+            medString = maxString;
+            medCount = maxCount;
 
-      medString = curString;
-      medCount = curCount;
+            maxString = curString;
+            maxCount = curCount;
+        }
+
+        return new LinkedList<String>(
+                Arrays.asList(maxString, medString, minString));
     }
 
-    if(isLarger(curCount, maxCount,curString.length(),maxString.length()))
-    {
-
-
-      medString = maxString;
-      medCount = maxCount;
-
-      maxString = curString;
-      maxCount = curCount;
+    /**
+     * method to do comparison for the current count against min med and max
+     * counts
+     */
+    boolean isLarger(int current, int compare, int lengthCurrent,
+            int lengthCompare) {
+        return current > compare
+                || (current == compare && lengthCurrent < lengthCompare);
     }
-
-  return new LinkedList<String>(Arrays.asList(maxString, medString,minString));
-  }
-  // method to do comparison for the current count against min med and max counts
-  boolean isLarger(int current, int compare, int lengthCurrent, int lengthCompare)
-  {
-      return current > compare || (current == compare && lengthCurrent < lengthCompare);
-  }
 }
